71a918daeef422a34d55c2ad71988dc7f30b46a2
SJ <shivanshjha93@gmail.com>
2025-01-29 00:01:41 +0530

    Create rev_integer.cpp

---
+++ Practice/rev_integer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <climits>
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10; // Extract the last digit
+            x /= 10;          // Remove the last digit
+            
+            // Check for overflow before updating `rev`
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) {
+                return 0; // Positive overflow
+            }
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) {
+                return 0; // Negative overflow
+            }
+            
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::cout << solution.reverse(-2147483648) << std::endl;
+    return 0;
+}
