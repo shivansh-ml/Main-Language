8022ecb04ace11e88e773d8792d159ba05e84ce9
SJ <shivanshjha93@gmail.com>
2025-02-06 22:53:26 +0530

    update

--- Recursion/Combination.cpp
+++ Recursion/Combination.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 using namespace std;
-// int C(int n, int r){
-//     int t1, t2, t3;
-//     t1 =  fact(n);
-//     t2 = fact(r);
-//     t3 = fact(n - r);
-//     return t1/(t2*t3);
-// } Using iteration
+// Using iteration
 
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    return fact(n - 1) * n;
+}
+
+int C(int n, int r)
+{
+    int t1, t2, t3;
+    t1 = fact(n);
+    t2 = fact(r);
+    t3 = fact(n - r);
+    return t1 / (t2 * t3);
+}
+
+int NCR(int n, int r){
+    if (n == r|| r==0){
+        return 1;
+    }
+    return NCR(n-1,r-1)+NCR(n-1,r);
+}
+
+int main(){
+    cout<<C(4, 2)<<endl;
+    cout<<NCR(4, 2)<<endl;
+}
