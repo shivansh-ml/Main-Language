72e3e68397eec4681f16a8516b15bb4fc1f42b26
SJ <shivanshjha93@gmail.com>
2025-10-30 20:10:24 +0530

    q

---
+++ LeetCode/Move_Zeroes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {0, 1, 0, 3, 12};
+    s.moveZeroes(nums);
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
